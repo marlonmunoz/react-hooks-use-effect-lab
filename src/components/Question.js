5fac47970290dd0763566f1e70bae1416d951769
Marlon Munoz <marlon@marlonshade.com>
2024-08-12 04:51:15 -0400

    hook useState lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) { // if time runs out, treat as incorrect answer
+      setTimeRemaining(10); // reset timer back to 10
+      onAnswered(false); // call onAnswered with false to notify parent component
+                          // that the answer was incorrect
+    }
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
+    const timerId = setTimeout(() => { // set up timer to decrement timeRemaining
+      setTimeRemaining((time) => time - 1); // decrement timeRemaining by 1
+    }, 1000); // run every 1000ms (1 second)
+    return () => clearTimeout(timerId); // clean up timer when component unmounts
+  }, [timeRemaining, onAnswered]); // re-run effect when timeRemaining or onAnswered changes
+  
+
+
+  function handleAnswer(isCorrect) { 
+    setTimeRemaining(10); 
+    onAnswered(isCorrect);  
   }
 
   const { id, prompt, answers, correctIndex } = question;
